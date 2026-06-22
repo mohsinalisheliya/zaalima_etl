1b962e6bb407d1b917f05abcf904b8f809fd2830
Sidram <sidramchalageri7@gmail.com>
2026-06-22 23:11:10 +0530

    feat: connected to real PostgreSQL database

--- load/database.py
+++ load/database.py
@@ -1,9 +1,19 @@
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://user:password@localhost:5432/zaalima_db"
+)
 
 engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
