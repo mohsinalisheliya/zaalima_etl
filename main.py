d21c99a7a22e379b9c6609b5bf2ec5c1a27b89db
Sheliya Mohsinali <sheliyamohsin342@gmail.com>
2026-06-29 13:21:12 +0530

    feat: added permanent file logging

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from load import load_data
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("pipeline_execution.log"), 
+        logging.StreamHandler()
+    ]
 )
 
 def main():
@@ -15,14 +19,14 @@ def main():
     raw_data = fetch_payment_data()
 
     logging.info("Step 2: Transforming data (Hanna's Engine)")
-    # We pass date_column=None because the dummy API doesn't have dates right now
+    
     cleaned_data = clean_data(raw_data, date_column=None)
     logging.info(f"Cleaned data ready. {len(cleaned_data)} rows.")
 
     logging.info("Step 3: Loading data (Sidram's Engine)")
     load_data(cleaned_data)
 
-    logging.info("Pipeline execution finished successfully! 🎉")
+    logging.info("Pipeline execution finished successfully! ")
 
 if __name__ == "__main__":
     main()
